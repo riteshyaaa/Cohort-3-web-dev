1a4961206abc6e91ea5dc5a15ded5416542fbc1c
riuteshyaaa <riteshyaaa@gmail.com>
2025-02-18 19:03:24 +0530

    Linking frontend to the backend

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#Day- 06
+- install express 
+- install jsonwebtoken
+- create a token by useing jWt token
+- created Sign up , sign In and /me (get user information) endpoint 
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-const JWT_SECRET = "ritesh";
+const JWT_SECRET = "riteshhhh";
 
 // Middleware for parsing JSON request bodies
 
 app.use(express.json());
 const users = [];
 
-// Middleware for handling authentication
-app.post("/signUp", function (req, res) {
+// localhost:3000
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+
+})
+app.post("/signup", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
   users.push({
@@ -22,7 +26,7 @@ app.post("/signUp", function (req, res) {
   });
 });
 
-app.post("/signIn", function (req, res) {
+app.post("/signin", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
   let founduser = null;
@@ -32,32 +36,43 @@ app.post("/signIn", function (req, res) {
       founduser = users[i];
     }
   }
-  if (founduser === null) {
+  if (!founduser ) {
     return res.status(401).json({ message: "Invalid username or password" });
-  }
-  const token = jwt.sign({username}, JWT_SECRET);
+   }else {
+ 
+  const token = jwt.sign({username:founduser.username}, JWT_SECRET);
   res.json({
     message: "User authenticated successfully",
     token: token,
   });
+}
 });
 
-// Middleware for validating JWT tokens
-app.get("/me", (req, res) => {
-  token = req.headers.token;
-  if (!token) {
-    return res.status(401).json({ message: "No token provided" });
-  }
+function auth(req, res, next) {
+  const token = req.headers.token;
   const decodedData = jwt.verify(token, JWT_SECRET);
-//   console.log(decodedData);
-  if (!decodedData) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+
   if (decodedData.username) {
+    // req = {status, headers...., username, password, userFirstName, random; ":123123"}
+    req.username = decodedData.username;
+    next();
+  } else {
+    res.json({
+      message: "You are not logged in",
+    });
+  }
+}
+
+// Middleware for validating JWT tokens
+app.get("/me", auth, (req, res) => {
+  //   console.log(decodedData);
+
+  if (req.username) {
+    username = req.username;
     let founduser = null;
 
     for (let i = 0; i < users.length; i++) {
-      if (users [i].username === decodedData.username) {
+      if (users[i].username === username) {
         founduser = users[i];
       }
     }
@@ -72,6 +87,11 @@ app.get("/me", (req, res) => {
     });
   }
 });
+app.post("/logout", (req, res) => {
+  
+
+  res.json({ message: "User logged out successfully" });
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
